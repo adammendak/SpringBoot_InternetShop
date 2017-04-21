f77a0c250345f87ef852e05c6a1918041eca8898
Adam Mendak <adam.mendak@gmail.com>
2017-04-21 15:17:57 +0200

    changes

--- main/urls.py
+++ main/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     url(r'^list$', views.index, name='index'),
     url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
     url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
-    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='views')
+    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote')
 ]
--- main/views.py
+++ main/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 from django.http import Http404
 #from django.template import loader
 
-from .models import Question
+from .models import Question, Choice
 
 def home(request):
     return render(request, "main/home.html",
@@ -41,10 +42,10 @@ def helloWorld(request):
 #     except Question.DoesNotExist:
 #         raise Http404("Question does not exist")
 #     return render(request, 'main/detail.html', {'question' : question})
-#
-# def detail(request, question_id):
-#     question = get_object_or_404(Question, pk=question_id)
-#     return render(request, "main/detail.html", {'question' : question})
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "main/detail.html", {'question' : question})
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
@@ -52,7 +53,7 @@ def vote(request, question_id):
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
     except (KeyError, Choice.DoesNotExist):
         # Redisplay the question voting form.
-        return render(request, 'polls/detail.html', {
+        return render(request, 'main/detail.html', {
             'question': question,
             'error_message': "You didn't select a choice.",
         })
@@ -64,11 +65,15 @@ def vote(request, question_id):
         # user hits the Back button.
         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
 
-
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'main/results.html', {'question': question})
 
-def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+
+# def results(request, question_id):
+#     response = "You're looking at the results of question %s."
+#     return HttpResponse(response % question_id)
+
+# def vote(request, question_id):
+#     return HttpResponse("You're voting on question %s." % question_id)
 
