3dae0f04b3a201038f506c0ab3957978aae75992
adammendak <adam.mendak@gmail.com>
2017-09-24 17:32:02 +0200

    changes

---
+++ src/main/java/src/com/adammendak/ProductRepository/ProductRepositoryApplication.java
@@ -0,0 +1,11 @@
+package src.com.adammendak.ProductRepository;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductRepositoryApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ProductRepositoryApplication.class,args);
+    }
+}
