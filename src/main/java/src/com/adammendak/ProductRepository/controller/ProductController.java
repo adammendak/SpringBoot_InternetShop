d54a2db295c4d83c9a079e6f0a5fd7eb17a7244d
adammendak <adam.mendak@gmail.com>
2017-09-27 20:42:02 +0200

    new branch

--- src/main/java/src/com/adammendak/ProductRepository/controller/ProductController.java
+++ src/main/java/src/com/adammendak/ProductRepository/controller/ProductController.java
@@ -3,15 +3,11 @@ package src.com.adammendak.ProductRepository.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import src.com.adammendak.ProductRepository.model.Product;
 import src.com.adammendak.ProductRepository.repository.ProductRepository;
-import sun.plugin.perf.PluginRollup;
-
-import java.util.ArrayList;
 
 @Controller
 public class ProductController {
