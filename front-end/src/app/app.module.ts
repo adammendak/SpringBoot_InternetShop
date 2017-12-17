1621c14456671488818327d7bf6690d3f8182e55
adammendak <adam.mendak@gmail.com>
2017-12-17 17:24:43 +0100

    adding anular material

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+// import {MatButtonModule, MatCheckboxModule} from '@angular/material';
 
 
 import { AppComponent } from './app.component';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
+import 'hammerjs';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { MaterialModule } from './material.module';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    BrowserAnimationsModule,
+    // MatCheckboxModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ front-end/src/app/nav-bar/nav-bar.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { MatToolbar} from '@angular/material';
+
+@Component({
+  selector: 'app-nav-bar',
+  templateUrl: './nav-bar.component.html',
+  styleUrls: ['./nav-bar.component.css']
+})
+export class NavBarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
