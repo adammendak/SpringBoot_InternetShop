8ddf9ea289eff05f65b5e70a51526cbc61e5559f
adammendak <adam.mendak@gmail.com>
2017-12-17 19:39:47 +0100

    changed to bootstrap

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -1,15 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-// import {MatButtonModule, MatCheckboxModule} from '@angular/material';
 
 
 import { AppComponent } from './app.component';
-import {FormsModule} from '@angular/forms';
-import {HttpModule} from '@angular/http';
-import 'hammerjs';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
-import { MaterialModule } from './material.module';
 
 
 @NgModule({
@@ -18,12 +12,7 @@ import { MaterialModule } from './material.module';
     NavBarComponent
   ],
   imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule,
-    BrowserAnimationsModule,
-    // MatCheckboxModule,
-    MaterialModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
